6b6a90fda57c97a60c8a2d7b8c3c1868f975a9c6
DefenderofJustice <30804477+DefenderofJustice@users.noreply.github.com>
2017-08-16 01:24:41 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
 confirm("hello");
 prompt("wanna join black knights?");
-var element= document.getelementById('main-text');
+
+var element= document.getElementById('main-text');
 element.innerHTML= "New value";
