5c15af015d7f9a5e0c8c71f26b22b2df444a146d
DefenderofJustice <30804477+DefenderofJustice@users.noreply.github.com>
2017-08-15 14:24:30 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+  title:"article-one|black knights",
+  heading:"article one of the order of black knights",
+  law:"C.C law",
+  content: ` <p>
+             This is the law which states that C.C. must get a pizza every 5 hours!!!! She must participate in ashford academy's largest pizza event every year. If due to unforseen coincidental circumstances the pizza is delayed she must evacuate the building immediately before king charles li britannia and queen marianne and v.v. and the code of geass finds her. 
+         </p>
+         <p>
+             In case C.C. is found wandering around she must be lured in by pizza. If by chance suzaku shows up he must get a series of shock images by c.c. immediately. It will confuse him for a while but you'll be able to escape. Oh and there is no danger to c.c as she cannot die! but doesn't mean you can cut her up like mao wanted to. That damn sadist.
+         </p>
+         <p>
+             In case C.C. is found unexpectedly staring at you. You must know that she infact doesn't care about it. She is a witch whose sole purpose is to eat pizza. If C.C. hears me she might give me geass. Ugh, the deathly power. I must stay clear of even touching c.c. Though I sometimes wonder hwat kind of shock wave images I might receive. Ugh Ugh. Lulu only you can save us ;~;
+         </p> `
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var law=data.law;
+    var content=data.content;
+var htmlTemplate=`
+<html>
+ <head>
+     <title>
+         ${title}
+     </title>
+     <meta name="viewport" content="width=device-width, initial-scale=1"/>
+     <link href="/ui/style.css" rel="stylesheet" />
+ </head>
+ <body>
+     <div class="container">
+     <div>
+         <a href ="/">Home</a>
+     </div>
+     <hr/>
+     <h3>
+         ${heading}
+     </h3>
+     <div>
+         ${law}
+     </div>
+     <div>
+        ${content}
+     </div>
+     </div>
+ </body>
+</html>
+`;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleone));
 });
 app.get('/article-two', function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
