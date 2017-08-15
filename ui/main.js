5c24088be87b57a3934fbad63e980bc9b52b7a18
DefenderofJustice <30804477+DefenderofJustice@users.noreply.github.com>
2017-08-16 01:29:13 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,8 @@ prompt("wanna join black knights?");
 
 var element= document.getElementById('main-text');
 element.innerHTML= "New value";
+
+var img=document.getElementById('madi');
+img.onclick=function(){
+ img.style.marginLeft='100px';
+};
