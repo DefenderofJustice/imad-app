c2f0be1234a642d660c151f5afda37bb0eecad24
DefenderofJustice <30804477+DefenderofJustice@users.noreply.github.com>
2017-08-22 23:56:36 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,7 +41,7 @@ span.innerHTML=counter.toString();
 
 // submit name
 var nameInput=document.getElementById('name');
-var name=nameInput.value;
+var name = nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
