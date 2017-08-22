9c10e8166fe6e2383ba57fea9f5704777806b78a
DefenderofJustice <30804477+DefenderofJustice@users.noreply.github.com>
2017-08-22 23:15:43 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/counter', function(req,res)
 });
 var names=[];
 
-app.get('/submit-name/',function(req,res){
+app.get('/submit-name',function(req,res){
 //get the name from the request object
 var name=req.query.name;
 names.push(name);
