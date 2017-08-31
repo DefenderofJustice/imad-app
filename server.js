21234cdeb82a96f0715a9a0026b3c546f15f0fea
DefenderofJustice <30804477+DefenderofJustice@users.noreply.github.com>
2017-08-31 09:30:44 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
 
+var config={
+    user: 'vaishaliagarwal2010',
+    database:'vaishaliagarwal2010',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:'process.env.DB_PASSWORD'
+};
 var app = express();
 app.use(morgan('combined'));
 var articles={
@@ -97,6 +105,20 @@ app.get('/counter', function(req,res)
     counter=counter+1;
     res.send(counter.toString());
 
+});
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * from test',function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    
+    }
+    else{
+        res.send(JSON.stringify(result));
+    }
+    });
 });
 var names=[];
 
